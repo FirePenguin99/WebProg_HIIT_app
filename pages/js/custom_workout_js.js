e86d87dae645696fbe1a75b96bd7e8ed2140f503
FirePenguin99 <gabriellewington@gmail.com>
2024-04-20 23:25:42 +0100

    added redirect to choose workout page after submitting a new customised workout

--- pages/js/custom_workout_js.js
+++ pages/js/custom_workout_js.js
@@ -340,6 +340,7 @@ async function submitWorkout() {
   });
 
   if (response.ok) {
+    window.location.href = 'choose_workout.html';
     console.log('huzzar!');
   } else {
     console.log('failed to send message');
